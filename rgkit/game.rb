3bc8393d685e1aab6c4f08329b2ed262e65452ee
Iliyan Bobev <bobev18@gmail.com>
2013-12-02 01:16:20 +0200

    refactored game_dict to game_hash

--- rgkit/game.rb
+++ rgkit/game.rb
@@ -3,20 +3,33 @@ require './board'
 require './bot'
 
 class Game
-  attr_accessor :player1, :player2, :board, :max_turns
+  attr_accessor :player1, :player2, :board, :max_turns, :players
   def initialize config
     @config = config
     @player1 = Player.new 1, "#FF0000"
     @player2 = Player.new 2, "#0000FF"
     @board = Board.new config
     @max_turns = 100
+    @players = [@player1, @player2]
   end
   
   def process_turn
   end
   
-  def get_game_dict
-    game = {robots: {}}
+  def game_hash for_player = nil
+    game = {}
+    @players.each do |player|
+      player.bots.each do |bot|
+        game[bot.location] = {
+          location: bot.location,
+          hp: bot.hp,
+          player_id: player.id
+        }
+        game[bot.location][:robot_id] = bot.bot_id unless for_player and
+          bot.player_id != for_player.id
+      end
+    end
+    {robots: game}
   end
   
   def spawn_one coodrs, player
@@ -24,7 +37,6 @@ class Game
   end
 
   def spawn
-    players = [@player1, @player2]
     pool = @board.spawn_coord.sample(5*players.size)
     pool.each_slice(5).zip(players) do |data|
       data[0].each { |coords| data[1].bots << spawn_one(coords, data[1]) }
--- rgkit/specs/spec_game.rb
+++ rgkit/specs/spec_game.rb
@@ -14,17 +14,14 @@ describe Game do
     @game.board.should be_an_instance_of Board
     @game.max_turns.should eq @config.max_turns
   end
+  it "can provide 'game' hash" do
+    @game.game_hash.should be_an_instance_of Hash
+  end
+  it "can provide 'game' hash for specific player" do
+    @game.game_hash(@game.player2).should be_an_instance_of Hash
+  end
   it "can process turns" do
     @game.public_methods.should include(:process_turn)
-  end
-  it "can generate 'game' dict" do
-    game_dict = @game.get_game_dict
-    game_dict.should be_an_instance_of Hash
-    game_dict.should include(:robots)
-    game_dict[:robots].size.should eq @game.player1.bots.size + @game.player1.bots.size
-    @game.spawn
-    game_dict = @game.get_game_dict
-    game_dict[:robots].size.should eq @game.player1.bots.size + @game.player1.bots.size
   end
   it "can spawn robots" do
     old_p1_bots = Array.new @game.player1.bots
@@ -39,5 +36,86 @@ describe Game do
   end
   after(:each) do
     @game = nil
+    @config = nil
+  end
+end
+
+describe Game do
+  before(:each) do
+    @config = Configuration.new
+    @game = Game.new @config
+    @game_hash = @game.game_hash
+  end
+  it "game hash has key :robots" do
+    @game_hash.should include(:robots)
+  end
+  it "game hash includes bots of all players" do
+    @game_hash[:robots].size.should eq @game.player1.bots.size + @game.player1.bots.size
+  end
+  it "game hash reflects spawn events" do
+    old_game_hash = Hash.new(@game_hash)
+    @game.spawn
+    @game_hash = @game.game_hash
+    @game_hash.should_not eq old_game_hash
+    @game_hash[:robots].size.should eq @game.player1.bots.size + @game.player1.bots.size
+  end
+  after(:each) do
+    @game = nil
+    @config = nil
+    @game_hash = nil
+  end
+end
+
+describe Game do
+  before(:each) do
+    @config = Configuration.new
+    @game = Game.new @config
+    @game.spawn
+    @game_hash = @game.game_hash
+  end
+  it "game hash has entries unique by player and robot ids" do
+    ids = @game_hash[:robots].values.map { |bot| [bot[:player_id], bot[:robot_id]] }
+    ids.should eq ids.uniq
+  end
+  it "game hash has bot location matching configuration spawn locations after initial spawn" do
+    sample_bot = @game_hash[:robots].values.sample
+    @config.spawns.should include(sample_bot[:location])
+  end
+  it "game hash has bot hit points matching configuration max_hp after initial spawn" do
+    sample_bot = @game_hash[:robots].values.sample
+    sample_bot[:hp].should eq @config.max_hp
+  end
+  after(:each) do
+    @game = nil
+    @config = nil
+    @game_hash = nil
+  end
+end
+
+describe Game do
+  before(:each) do
+    @config = Configuration.new
+    @game = Game.new @config
+    @game.spawn
+    @old_player1_bot_ids = Set.new(@game.player1.bots.map(&:bot_id))
+    @game.player1.bots = []
+    center = [[9,9], [9, 10], [9,8], [8,9], [10,9]]
+    @game.player2.bots.zip(center).map { |bot_loc| bot_loc[0].location = bot_loc[1] }
+    @game.spawn
+    @game_hash = @game.game_hash @game.player1
+  end
+  it "game hash has robot_id only if it belongs to the player" do
+    # @game_hash[:robots].values.sort_by { |b| b[:player_id] }.each_with_index { |b, i| puts "#{i} #{b}" }
+    player1_hash_bots = @game_hash[:robots].values.select { |bot| bot.keys.include? :robot_id }
+    player1_hash_bots.map { |bot| bot[:robot_id] }.should =~ @game.player1.bots.map(&:bot_id)
+  end
+  # it "doesn't reuse robot_id values" do
+  #   (@old_player1_bot_ids & @game.player1.bots.map(&:bot_id)).should eq Set.new
+  # end
+  after(:each) do
+    @config = nil
+    @game = nil
+    @game_hash = nil
+    @old_player1_bot_ids = nil
   end
 end
