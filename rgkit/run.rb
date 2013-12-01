28f5111f466c92c6fbc18014dbdfeeeaaf23ce9a
Iliyan Bobev <bobev18@gmail.com>
2013-12-01 21:25:18 +0200

    implemented run config

--- rgkit/run.rb
+++ rgkit/run.rb
@@ -1,3 +1,9 @@
-class Run
-end
+require 'yaml'
+CONFIG = YAML.load_file('config.yml')
 
+class Run
+  attr_accessor :config
+  def initialize config = 'default'
+    @config = CONFIG[config]
+  end
+end
